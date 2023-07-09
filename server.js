edba662d7e8b395f4481f0ed9c2c3de448fbbcb5
shashank934602 <93077450+shashank934602@users.noreply.github.com>
2023-07-10 02:44:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ import categoryRoutes from './routes/categoryRoutes.js'
 import productRoutes from './routes/productRoutes.js'
 import cors from "cors"
 import path from 'path';
+import {fileURLToPath}from'url';
 //configure env
 dotenv.config();
 
@@ -22,6 +23,10 @@ connectDB();
 
 const app = express()
 
+
+//es moduole 6
+const __filename=fileURLToPath(import.meta.url);
+const __dirname=path.dirname(__filename);
 
 //midlewares
 app.use(cors())
